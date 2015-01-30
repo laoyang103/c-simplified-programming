954ad953ee4d3fb38a31b69637e2456a3df7c837
laoyang103 <553069938@qq.com>
2015-01-30 23:29:30 +0800

    文件夹备份练习

--- shell-study/usage.sh
+++ shell-study/usage.sh
@@ -10,3 +10,17 @@ grep ^$user $passwddb > /dev/null
 
 res=$(grep ^$user $passwddb)
 [ ! -z $res ] && echo "$user found" || echo "$user not found"
+
+SOURCE=/etc/apt
+TOBACKUP=/tmp/etc-backup
+ERRORLOG=/tmp/error.log
+
+[ ! -d $SOURCE ] && { echo "$SOURCE not found ..."; exit 1; }
+[ ! -d $TOBACKUP ] && { echo "$TOBACKUP not found, create it ..."; mkdir -p $TOBACKUP; }
+
+tar -zcvf $TOBACKUP"/etc-backup.tar.gz" $SOURCE 2>$ERRORLOG
+
+if [ $? -ne 0 ]; then
+    echo "An error occurred during backup ses $ERRORLOG"
+    exit 2
+fi
